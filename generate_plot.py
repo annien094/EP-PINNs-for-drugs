b45fc9c76fd6a258d025eb50a17a51ea6ca3e80b
annien094 <annien094@gmail.com>
2023-11-09 10:09:09 +0000

    png instead of tiff

--- generate_plot.py
+++ generate_plot.py
@@ -3,8 +3,8 @@
 """
 Generate plots by calling the plot_1D function in the main file. 
 This includes plotting: 
-    potential across time at a particular cell (plot_1D_cell),
-    potential across cells (spatially) at a particular time (plot_1D_array),
+    potential u across time at a particular cell (plot_1D_cell),
+    potential u across cells (spatially) at a particular time (plot_1D_array),
     and potential across time and space (plot_1D_grid).
 The function takes inputs:
     data_list, dynamics, model, fig_name, x_t_to_plot, i.
@@ -22,12 +22,13 @@ def plot_1D(data_list, dynamics, model, fig_name, x_t_to_plot, i):
     plot_1D_cell(data_list, dynamics, model, fig_name[1:], x_t_to_plot[0], i)
     plot_1D_array(data_list, dynamics, model, fig_name[1:], x_t_to_plot[1], i)
     plot_1D_grid(data_list, dynamics, model, fig_name[1:], x_t_to_plot, i)
+    # plot_1D_vw(data_list, dynamics, model, fig_name[1:], x_t_to_plot, i)
     return 0
     
 def plot_1D_cell(data_list, dynamics, model, fig_name, x_to_plot, i):
     
      ## Unpack data
-    observe_x, observe_train, v_train, v, observe_test, v_test= \
+    observe_x, observe_train, u_train, u, observe_test, u_test= \
           data_list[0], data_list[1], data_list[2], data_list[3], data_list[4], data_list[5]
     
     ## Pick a cell to show
@@ -40,35 +41,35 @@ def plot_1D_cell(data_list, dynamics, model, fig_name, x_to_plot, i):
     ## Get data for cell
     idx = [i for i,ix in enumerate(observe_x) if observe_x[i][0]==cell]
     observe_geomtime = observe_x[idx]
-    v_GT = v[idx]
-    v_predict = model.predict(observe_geomtime)[:,0:1]
+    u_GT = u[idx]
+    u_predict = model.predict(observe_geomtime)[:,0:1]
     t_axis = observe_geomtime[:,1]
     
     ## Get data for points used in training process
     idx_train = [i for i,ix in enumerate(observe_train) if observe_train[i][0]==cell]
-    v_trained_points = v_train[idx_train]
+    u_trained_points = u_train[idx_train]
     t_markers = (observe_train[idx_train])[:,1]
 
     ## Get data for points used in testing
     idx_test = [i for i,ix in enumerate(observe_test) if observe_test[i][0]==cell]
-    v_test_points = v_test[idx_test]
+    u_test_points = u_test[idx_test]
     t_markers_test = (observe_test[idx_test])[:,1]
     
     ## create figure for cell
     plt.figure()
     plt.rc('font', size= ftsz) #controls default text
     fig, ax = plt.subplots()
-    Predicted, = ax.plot(t_axis, v_predict, c='r', label='Predicted',linewidth=lnw, zorder=0)
-    GT, = ax.plot(t_axis, v_GT, c='b', label='GT',linewidth=lnw, linestyle = 'dashed', zorder=5)
+    Predicted, = ax.plot(t_axis, u_predict, c='r', label='Predicted',linewidth=lnw, zorder=0)
+    GT, = ax.plot(t_axis, u_GT, c='b', label='GT',linewidth=lnw, linestyle = 'dashed', zorder=5)
     #plt.plot(t_axis, v_GT, c='b', label='GT')
     #plt.plot(t_axis, v_predict, c='r', label='Predicted')
     # If there are any trained data points for the current cell 
     if len(t_markers):
-        ax.scatter(t_markers, v_trained_points, marker='x', c='black',s=szm, label='Training points', zorder=10)
-    plt.legend(loc='upper right', borderpad=0.2)
+        ax.scatter(t_markers, u_trained_points, marker='x', c='black',s=szm, label='Training points', zorder=10)
+    #plt.legend(loc='upper right', borderpad=0.2)
     #if there are any test points for the current cell
     if len(t_markers_test):
-        ax.scatter(t_markers_test, v_test_points, marker='x', c='green',s=szm, label='Testing points',zorder=10)
+        ax.scatter(t_markers_test, u_test_points, marker='x', c='green',s=szm, label='Testing points',zorder=10)
 
     plt.title("_run_" + str(i) + "cell_at" + str(x_to_plot) + "mm")
     plt.xlabel('t (TU)', fontsize = ftsz)
@@ -76,11 +77,12 @@ def plot_1D_cell(data_list, dynamics, model, fig_name, x_to_plot, i):
     plt.ylim((-0.2,1.2))
     
     ## save figure
-    png1 = io.BytesIO()
-    plt.savefig(png1, format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
-    png2 = Image.open(png1)
-    png2.save("run_" + str(i) + "cell_at" + str(x_to_plot) + "mm_plot_1D.tiff")
-    png1.close()
+    plt.savefig("run_" + str(i) + "cell_at" + str(x_to_plot) + "mm_plot_1D.png", format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
+    # png1 = io.BytesIO()
+    # plt.savefig(png1, format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
+    # png2 = Image.open(png1)
+    # png2.save("run_" + str(i) + "cell_at" + str(x_to_plot) + "mm_plot_1D.tiff")
+    # png1.close()
     return 0
 
 def plot_1D_array(data_list, dynamics, model, fig_name, t_to_plot, i):
@@ -89,7 +91,7 @@ def plot_1D_array(data_list, dynamics, model, fig_name, t_to_plot, i):
     observe_x, observe_train, v_train, v = data_list[0], data_list[1], data_list[2], data_list[3]
     
     ## Pick a point in time to show
-    obs_t = dynamics.max_t*t_to_plot
+    obs_t = round(dynamics.max_t*t_to_plot)
     lnw = 3.0 # line width
     szm = 26 # marker size
     ftsz = 20 # font size    
@@ -120,12 +122,14 @@ def plot_1D_array(data_list, dynamics, model, fig_name, t_to_plot, i):
     plt.ylim((-0.2,1.2))
     
     ## save figure
-    png1 = io.BytesIO()
-    plt.savefig(png1, format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
-    png2 = Image.open(png1)
-    png2.save("run_" + str(i) + "_array_plot_1D.tiff")
-    png1.close()
+    plt.savefig("run_" + str(i) + "_array_plot_1D.png", format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
+    # png1 = io.BytesIO()
+    # plt.savefig(png1, format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
+    # png2 = Image.open(png1)
+    # png2.save("run_" + str(i) + "_array_plot_1D.tiff")
+    # png1.close()
     return 0
+
 
 def plot_1D_grid(data_list, dynamics, model, fig_name, x_t_to_plot, i):
     
@@ -140,14 +144,14 @@ def plot_1D_grid(data_list, dynamics, model, fig_name, x_t_to_plot, i):
     data = np.hstack((X_data, T_data))
     v_pred = model.predict(data)[:,0:1]
     Z = np.zeros((grid_size,grid_size))
-    for i in range(grid_size):
-        Z[i,:] = (v_pred[(i*grid_size):((i+1)*grid_size)]).reshape(-1)
+    for j in range(grid_size):
+        Z[j,:] = (v_pred[(j*grid_size):((j+1)*grid_size)]).reshape(-1)
     
     ## create figure
     plt.figure()
     contour = plt.contourf(T,X,Z, levels = np.arange(-0.15,1.06,0.15) , cmap=plt.cm.bone)
     plt.plot(t, np.full(len(t), 2), '--', label = 'slice across t')
-    plt.vlines(dynamics.max_t * x_t_to_plot[1], ymin = 0, ymax = dynamics.max_x,
+    plt.vlines(round(dynamics.max_t * x_t_to_plot[1]), ymin = 0, ymax = dynamics.max_x,
            colors = 'purple', linestyles = '--', label = 'slice across x')
     plt.xlabel('t')
     plt.ylabel('x')
@@ -156,11 +160,12 @@ def plot_1D_grid(data_list, dynamics, model, fig_name, x_t_to_plot, i):
     cbar.ax.set_ylabel('Membrane Potential U (AU)')
     
     ## save figure
-    png1 = io.BytesIO()
-    plt.savefig(png1, format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
-    png2 = Image.open(png1)
-    png2.save("run_" + str(i) + "_grid_plot_1D.tiff")
-    png1.close()
+    plt.savefig("run_" + str(i) + "_grid_plot_1D.png", format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
+    # png1 = io.BytesIO()
+    # plt.savefig(png1, format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
+    # png2 = Image.open(png1)
+    # png2.save("run_" + str(i) + "_grid_plot_1D.tiff")
+    # png1.close()
     
     return 0
     
